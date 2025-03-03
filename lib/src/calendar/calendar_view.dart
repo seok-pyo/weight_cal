0726f2b9b7e21253a25ccd3229bd8cba65840f24
hongseokpyo <hongseokpyou@gmail.com>
2025-03-03 21:20:36 +0900

    calendar app start setting

---
+++ lib/src/calendar/calendar_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:weight_cal/src/settings/settings_view.dart';
+
+class CalendarView extends StatelessWidget {
+  final DateTime startDate = DateTime(2025, 1, 1);
+  final DateTime endDate = DateTime(2025, 12, 31);
+
+  List<DateTime> generateDates(DateTime start, DateTime end) {
+    List<DateTime> dates = [];
+    for (int i = 0; i <= end.difference(start).inDays; i++) {
+      dates.add(start.add(Duration(days: i)));
+    }
+    return dates;
+  }
+
+  static const routeName = '/';
+
+  CalendarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // List<DateTime> dates = generateDates(startDate, endDate);
+    int startDateNum = 3;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('2025 Calendar'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 7,
+          childAspectRatio: 1,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ),
+        itemCount: 31 + startDateNum,
+        itemBuilder: (context, index) {
+          // DateTime date = dates[index];
+          if (index < startDateNum) {
+            return Container();
+          }
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(
+                (index - startDateNum + 1).toString(),
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
