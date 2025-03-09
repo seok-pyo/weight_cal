369161d124a46f5ed11f1455f7d7c66a58bf6f4f
hongseokpyo <hongseokpyou@gmail.com>
2025-03-10 01:28:13 +0900

    hive init fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:weight_cal/src/provider/weight_provider.dart';
 import 'package:weight_cal/src/theme/theme_controller.dart';
@@ -11,6 +12,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // hive init
+  final directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   await Hive.initFlutter();
 
   // theme
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''),
+            Locale('kor', ''),
           ],
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
